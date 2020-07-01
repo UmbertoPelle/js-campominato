b38bd8b7a678d36ca0f992b68b96aa245c6b1965
Umberto <umbertopellegrino96@gmail.com>
2020-07-01 12:08:44 +0200

    Bonus

--- js/jscript.js
+++ js/jscript.js
@@ -1,58 +1,134 @@
 var arrayPc = [];
 var arrayUtente = [];
+var btnDiff =document.getElementById("btn");
 var listaPc= document.getElementById("listaPc");
 var listaUtente= document.getElementById("listaUtente");
 var errore = document.getElementById("errore");
 var win = document.getElementById("win");
 
+btnDiff.addEventListener("click",function(){
+  var difficoltà= document.getElementById("difficoltà").value;
+  if (difficoltà==0) {
+    // inserimento numeri array pc
+    while (arrayPc.length < 16) {
+      var numPc = randomNum(1,100);
+      if (!(insideCheck(arrayPc,numPc))) {
+        arrayPc.push(numPc);
+        listaPc.innerHTML += "<li>" + numPc + "</li>" ;
+      }
+    }
+    console.log("Lista PC: ",arrayPc);
 
-// inserimento numeri array pc
-while (arrayPc.length < 16) {
-  var numPc = randomNum(1,100);
-  if (!(insideCheck(arrayPc,numPc))) {
-    arrayPc.push(numPc);
-    listaPc.innerHTML += "<li>" + numPc + "</li>" ;
+    // inserimento numero lista utente
+    var trovato = false;
+    var numUtente = 0;
+    while (arrayUtente.length < 5 && trovato == false) {
+      numUtente = parseInt(prompt("inserisci un numero da 1 a 100:"));
+
+      while (range(numUtente,1,100) == false) {
+        numUtente = parseInt(prompt("Devi inserire un valore da 1 a 100:"));
+      }
+
+      if (insideCheck(arrayPc, numUtente)) {
+        trovato = true;
+        errore.innerHTML = "Hai perso , il numero inserito è una bomba ->"+numUtente;
+      }
+
+      while (insideCheck(arrayUtente, numUtente)) {
+        numUtente = parseInt(prompt("dato già presente, riprova:"));
+      }
+
+      if (insideCheck(arrayUtente,numUtente) == false && insideCheck(arrayPc, numUtente) == false) {
+        arrayUtente.push(numUtente);
+        listaUtente.innerHTML += "<li>" + numUtente + "</li>" ;
+      }
+    }
+
+
+  } else if (difficoltà==1) {
+    // inserimento numeri array pc
+    while (arrayPc.length < 16) {
+      var numPc = randomNum(1,80);
+      if (!(insideCheck(arrayPc,numPc))) {
+        arrayPc.push(numPc);
+        listaPc.innerHTML += "<li>" + numPc + "</li>" ;
+      }
+    }
+    console.log("Lista PC: ",arrayPc);
+
+    // inserimento numero lista utente
+    var trovato = false;
+    var numUtente = 0;
+    while (arrayUtente.length < 5 && trovato == false) {
+      numUtente = parseInt(prompt("inserisci un numero da 1 a 80:"));
+
+      while (range(numUtente,1,80) == false) {
+        numUtente = parseInt(prompt("Devi inserire un valore da 1 a 80:"));
+      }
+
+      if (insideCheck(arrayPc, numUtente)) {
+        trovato = true;
+        errore.innerHTML = "Hai perso , il numero inserito è una bomba ->"+numUtente;
+      }
+
+      while (insideCheck(arrayUtente, numUtente)) {
+        numUtente = parseInt(prompt("dato già presente, riprova:"));
+      }
+
+      if (insideCheck(arrayUtente,numUtente) == false && insideCheck(arrayPc, numUtente) == false) {
+        arrayUtente.push(numUtente);
+        listaUtente.innerHTML += "<li>" + numUtente + "</li>" ;
+      }
+    }
+
+
+  } else {
+    // inserimento numeri array pc
+    while (arrayPc.length < 16) {
+      var numPc = randomNum(1,50);
+      if (!(insideCheck(arrayPc,numPc))) {
+        arrayPc.push(numPc);
+        listaPc.innerHTML += "<li>" + numPc + "</li>" ;
+      }
+    }
+    console.log("Lista PC: ",arrayPc);
+
+    // inserimento numero lista utente
+    var trovato = false;
+    var numUtente = 0;
+    while (arrayUtente.length < 5 && trovato == false) {
+      numUtente = parseInt(prompt("inserisci un numero da 1 a 50:"));
+
+      while (range(numUtente,1,50) == false) {
+        numUtente = parseInt(prompt("Devi inserire un valore da 1 a 50:"));
+      }
+
+      if (insideCheck(arrayPc, numUtente)) {
+        trovato = true;
+        errore.innerHTML = "Hai perso , il numero inserito è una bomba ->"+numUtente;
+      }
+
+      while (insideCheck(arrayUtente, numUtente)) {
+        numUtente = parseInt(prompt("dato già presente, riprova:"));
+      }
+
+      if (insideCheck(arrayUtente,numUtente) == false && insideCheck(arrayPc, numUtente) == false) {
+        arrayUtente.push(numUtente);
+        listaUtente.innerHTML += "<li>" + numUtente + "</li>" ;
+      }
+    }
+
+
   }
-}
-console.log("Lista PC: ",arrayPc);
-
-// inserimento numero lista utente
-var trovato = false;
-var numUtente = 0;
-while (arrayUtente.length < 5 && trovato == false) {
-  numUtente = parseInt(prompt("inserisci un numero da 1 a 100:"));
-
-  while (range(numUtente,1,100) == false) {
-    numUtente = parseInt(prompt("Devi inserire un valore da 1 a 100:"));
+  // condizione vittoria
+  if (arrayUtente.length == 5) {
+    win.innerHTML="Congratulazioni, hai vinto!!";
   }
-
-  if (insideCheck(arrayPc, numUtente)) {
-    trovato = true;
-    errore.innerHTML = "Hai perso , il numero inserito è una bomba ->"+numUtente;
-  }
-
-
-  while (insideCheck(arrayUtente, numUtente)) {
-    numUtente = parseInt(prompt("dato già presente, riprova:"));
-  }
-
-  if (insideCheck(arrayUtente,numUtente) == false && insideCheck(arrayPc, numUtente) == false) {
-    arrayUtente.push(numUtente);
-    listaUtente.innerHTML += "<li>" + numUtente + "</li>" ;
-  }
-
-
-}
+});
 
 
 
-// condizione vittoria
-if (arrayUtente.length == 5) {
-  win.innerHTML="Congratulazioni, hai vinto!!"
-}
 
-
-console.log("Lista UTENTE: ", arrayUtente);
 // functions
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
